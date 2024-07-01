c6fa247d3d45c7a7a7248b448cd7ecb87446839f
Bernhard Auer <52306591@edu.campus02.at>
2024-07-01 12:10:29 +0200

    Implement Constructor, peek() and element() for DrinkQueue.java

--- src/main/java/at/campus02/bsd/DrinkQueue.java
+++ src/main/java/at/campus02/bsd/DrinkQueue.java
@@ -1,4 +1,33 @@
 package at.campus02.bsd;
 
-public class DrinkQueue {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class DrinkQueue{
+
+    private List<Drink> elements = new ArrayList<>();
+    private int maxSize;
+
+
+    public DrinkQueue(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public Drink peek() {
+        if (elements.isEmpty()) {
+            return null;
+        } else {
+            return elements.get(0);
+        }
+    }
+
+    public Drink element() {
+        Drink element = peek();
+        if (element == null) {
+            throw new NoSuchElementException("there's no element any more");
+        } else {
+            return element;
+        }
+    }
 }
