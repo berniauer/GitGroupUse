d74f27974fdf6aab7884e8bcac63857d14e79af2
Bernhard Auer <52306591@edu.campus02.at>
2024-07-01 13:22:01 +0200

    Added Javadoc for Cocktail.java

--- src/main/java/at/campus02/bsd/Cocktail.java
+++ src/main/java/at/campus02/bsd/Cocktail.java
@@ -2,19 +2,35 @@ package at.campus02.bsd;
 
 import java.util.List;
 
+/**
+ * The {@code Cocktail} class represents a cocktail drink composed of various liquids.
+ * It extends the {@code Drink} class, inheriting its basic properties and adding a list
+ * of {@code Liquid} objects to represent the ingredients of the cocktail.
+ */
 public class Cocktail extends Drink{
+
+    /**
+     * The list of {@code Liquid} objects representing the ingredients of the cocktail.
+     */
     private List<Liquid> liquids;
 
     /**
-     * Creates a Drink object with given name
+     * Constructs a new {@code Cocktail} object with the specified name and list of liquids.
      *
-     * @param name name of the drink
+     * @param name    the name of the cocktail
+     * @param liquids a list of {@code Liquid} objects that are the ingredients of the cocktail
      */
     public Cocktail(String name, List<Liquid> liquids) {
         super(name);
         this.liquids = liquids;
     }
 
+
+    /**
+     * Calculates and returns the total volume of the cocktail based on the volume of each liquid ingredient.
+     *
+     * @return the total volume of all liquids in the cocktail in liters
+     */
     @Override
     public double getVolume() {
         double totalVolume = 0;
@@ -24,6 +40,12 @@ public class Cocktail extends Drink{
         return totalVolume;
     }
 
+    /**
+     * Calculates and returns the overall alcohol percentage of the cocktail.
+     * This is computed based on the alcohol percentage and volume of each liquid ingredient.
+     *
+     * @return the alcohol percentage of the cocktail
+     */
     @Override
     public double getAlcoholPercent() {
         double totalAlcoholVolume = 0;
@@ -33,6 +55,12 @@ public class Cocktail extends Drink{
         return totalAlcoholVolume / getVolume() * 100;
     }
 
+    /**
+     * Determines whether the cocktail is alcoholic.
+     * A cocktail is considered alcoholic if its overall alcohol percentage is greater than zero.
+     *
+     * @return {@code true} if the cocktail has an alcohol percentage greater than 0, otherwise {@code false}
+     */
     @Override
     public boolean isAlcoholic() {
         return getAlcoholPercent() > 0;
